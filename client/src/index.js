5fc159ae40764fd06f303cb3111700bbba9d3212
trachanh1609 <Vu@Users-MBP.home>
2017-06-20 19:37:08 +0300

    folder structure added, sample router added

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,23 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import {
+  HashRouter,
+  Route,
+  Link
+} from 'react-router-dom';
+import SubHeader from './components/SubHeader'
+
+ReactDOM.render((
+   <HashRouter>
+      <div>
+        <Route exact path="/" component={App} />
+        <Route path="/sub" component={SubHeader} />
+      </div>
+   </HashRouter>
+), document.getElementById( 'root' ))
+
+
 registerServiceWorker();
